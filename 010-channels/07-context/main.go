2bf7d0702a5ba9c1e5cb9f4478777673c1520cec
Vladlen Tereshchenko <vladlent.dev@gmail.com>
2021-01-16 22:43:37 +0200

    added fan-out and context examples to channels

---
+++ 010-channels/07-context/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func main() {
+	ctx := context.WithValue(context.Background(), "language", "Go")
+	foo(ctx, "language")
+
+	ctx = context.WithValue(ctx, "dog", "Gaston")
+	foo(ctx, "dog")
+
+	foo(ctx, "color")
+}
+
+func foo(ctx context.Context, s string) {
+	if v := ctx.Value(s); v != nil {
+		fmt.Println("found value:", v)
+		return
+	}
+	fmt.Println("key not found:", s)
+}
